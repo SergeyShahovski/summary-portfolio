872fa52d81dceae46935c5e1c860687607f18f78
Serhey <127224179+SergeyShahovski@users.noreply.github.com>
2023-03-19 14:15:03 +0300

    Add files via upload

---
+++ script/form.js
@@ -0,0 +1,17 @@
+document.getElementById('form_btn').onclick = function() {
+  document.getElementById('form').classList.add('hidden_form');
+}
+
+
+document.getElementById('close', 'btn').onclick = function() {
+  document.getElementById('form').classList.remove('hidden_form');
+}
+
+
+
+
+
+
+
+
+
---
+++ script/menu.js
@@ -0,0 +1,8 @@
+document.getElementById('menu_btn').onclick = function() {
+document.getElementById('menu').classList.add('hidden-menu');
+}
+
+
+document.getElementById('menu_btn').onclick = function() {
+document.getElementById('menu').classList.toggle('hidden-menu');
+}
